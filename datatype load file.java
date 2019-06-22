7d7f6eb72789b0d50b5039bf76ffbe89a224f261
Shouvik Sunil Sharma <shouvik19@gmail.com>
2019-06-22 11:17:12 +0530

    Update datatype load file.java
    input file using methods from class sort

--- datatype load file.java
+++ datatype load file.java
@@ -1,3 +1,4 @@
+
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -6,9 +7,12 @@ public class project
 
 public static void main(String[] args)
 {
-	// calling quick sort algo
-	QuickSort_comparable qc= new QuickSort_comparable();
 	
+	
+	// calling sorting methods from the class Sort_methods
+	Sort_methods qc= new Sort_methods();
+	
+	// Taking input from user about the data type of input
 	
 	Scanner scan = new Scanner(System.in); 	
 	System.out.println("please enter the datatype of your input from the following options :-");
@@ -16,15 +20,21 @@ public static void main(String[] args)
 	System.out.println("2. double");
 	System.out.println("3. String");
 	int input_datatype=scan.nextInt();
+	
+	// Number of inputs to be entered by the user
+	
 	System.out.println("Please enter the number of entries you wish to list");
 	int n;
     n = scan.nextInt();
 	
-		
-
+	// We have applied a switch statement base on the data type selected by the user
+	
 	switch (input_datatype)
 	{
-		case 1:
+	
+	// Integer Datatype
+	
+	case 1:
 		Integer a[] = new Integer[n];
         System.out.println("Enter all the elements:");
         for(int i = 0; i < n; i++)
@@ -32,7 +42,42 @@ public static void main(String[] args)
             a[i] = scan.nextInt();
         }
 		
-		long startTime1 = System.nanoTime();
+		
+		// User needs to specify the type of sorting algorithm he needs to used
+		
+		System.out.println("Please select a sorting algorithm:");
+		System.out.println("1. Bubble Sort");
+		System.out.println("2. Quick Sort");
+		int sort_choice;
+		sort_choice = scan.nextInt();
+		
+		
+		
+		switch (sort_choice)
+		{
+		
+		// Bubble Sort 
+		
+		case 1:
+		
+		long startTime_bubble_sort_1 = System.nanoTime();
+		
+		System.out.println("Original array : "+Arrays.asList(a));
+		qc.bubbleSort(a, 0, a.length-1);
+
+		System.out.println("Sorted array : "+Arrays.asList(a));
+		
+		System.out.print("total time in nano-seconds:");
+		System.out.println(System.nanoTime()-startTime_bubble_sort_1);
+		System.out.println("Number of comparisons "+qc.compincrement_1);
+		
+		break;
+	
+		// Quick sort
+	
+		case 2:
+		
+		long startTime_bubble_sort_2 = System.nanoTime();
 		
 		System.out.println("Original array : "+Arrays.asList(a));
 		qc.quickSort(a, 0, a.length-1);
@@ -40,12 +85,21 @@ public static void main(String[] args)
 		System.out.println("Sorted array : "+Arrays.asList(a));
 		
 		System.out.print("total time in nano-seconds:");
-		System.out.println(System.nanoTime()-startTime1);
+		System.out.println(System.nanoTime()-startTime_bubble_sort_2);
 		System.out.println("Number of comparisons "+qc.compincrement);
 
+		break;
+		
+		
+		default:
+		System.out.println("invalid entry");
+		}
 
+		
 		break;
 
+		// Double Datatype
+		
 		case 2:
 		Double[] a1 = new Double[n];
         System.out.println("Enter all the elements:");
@@ -53,8 +107,39 @@ public static void main(String[] args)
         {
             a1[i] = scan.nextDouble();
         }
+		System.out.println("Please select a sorting algorithm:");
+		System.out.println("1. Bubble Sort");
+		System.out.println("2. Quick Sort");
 
-		long startTime2 = System.nanoTime();
+		
+		int sort_choice_d;
+		sort_choice_d = scan.nextInt();
+
+		switch (sort_choice_d)
+		{
+		
+		// Bubble sort
+		
+		case 1:
+		
+		long startTime_bubble_sort_1_double = System.nanoTime();
+		
+		System.out.println("Original array : "+Arrays.asList(a1));
+		qc.bubbleSort(a1, 0, a1.length-1);
+
+		System.out.println("Sorted array : "+Arrays.asList(a1));
+		
+		System.out.print("total time in nano-seconds:");
+		System.out.println(System.nanoTime()-startTime_bubble_sort_1_double);
+		System.out.println("Number of comparisons "+qc.compincrement_1);
+		
+		break;
+	
+		// Quick sort
+	
+		case 2:
+		
+		long startTime_bubble_sort_2_double = System.nanoTime();
 		
 		System.out.println("Original array : "+Arrays.asList(a1));
 		qc.quickSort(a1, 0, a1.length-1);
@@ -62,10 +147,19 @@ public static void main(String[] args)
 		System.out.println("Sorted array : "+Arrays.asList(a1));
 		
 		System.out.print("total time in nano-seconds:");
-		System.out.println(System.nanoTime()-startTime2);
+		System.out.println(System.nanoTime()-startTime_bubble_sort_2_double);
 		System.out.println("Number of comparisons "+qc.compincrement);
 
 		break;
+		
+		
+		default:
+		System.out.println("invalid entry");
+		}
+		break;
+		
+		
+		//  String datatype
 		
 		case 3:
 		String[] unsortedArray = new String[n];
@@ -74,7 +168,39 @@ public static void main(String[] args)
         {
             unsortedArray[i] = scan.next();
         }
-		long startTime = System.nanoTime();
+		
+		System.out.println("Please select a sorting algorithm:");
+		System.out.println("1. Bubble Sort");
+		System.out.println("2. Quick Sort");
+		
+		int sort_choice_S;
+		sort_choice_S = scan.nextInt();
+		
+		switch (sort_choice_S)
+		{
+		
+		case 1:
+		
+		// Bubble sort 
+		
+		long startTime_bubble_sort_1_String = System.nanoTime();
+		
+		System.out.println("Original array : "+Arrays.asList(unsortedArray));
+		qc.bubbleSort(unsortedArray, 0, unsortedArray.length-1);
+
+		System.out.println("Sorted array : "+Arrays.asList(unsortedArray));
+		
+		System.out.print("total time in nano-seconds:");
+		System.out.println(System.nanoTime()-startTime_bubble_sort_1_String);
+		System.out.println("Number of comparisons "+qc.compincrement_1);
+		
+		break;
+	
+		case 2:
+		
+		// Quick sort 
+		
+		long startTime_bubble_sort_2_String = System.nanoTime();
 		
 		System.out.println("Original array : "+Arrays.asList(unsortedArray));
 		qc.quickSort(unsortedArray, 0, unsortedArray.length-1);
@@ -82,9 +208,16 @@ public static void main(String[] args)
 		System.out.println("Sorted array : "+Arrays.asList(unsortedArray));
 		
 		System.out.print("total time in nano-seconds:");
-		System.out.println(System.nanoTime()-startTime);
+		System.out.println(System.nanoTime()-startTime_bubble_sort_2_String);
 		System.out.println("Number of comparisons "+qc.compincrement);
 
+		break;
+		
+		
+		default:
+		System.out.println("invalid entry");
+		}
+	
 
 		break;
 		
@@ -93,7 +226,7 @@ public static void main(String[] args)
 
 		
 
+
 }
 }
 }
-		
